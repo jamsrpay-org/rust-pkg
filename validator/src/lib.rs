9bdbb3c523f9b0db981cd3449ec8fb25a7a2899c
adityaa72 <adityah1357908642@gmail.com>
2025-12-24 22:57:24 +0530

    feat: validator

--- validator/src/lib.rs
+++ validator/src/lib.rs
@@ -1,6 +1,6 @@
-use std::panic::Location;
-
-use tonic::Status;
+use std::{fmt::format, panic::Location, vec};
+use tonic::{Code, Status, metadata::MetadataMap};
+use tonic_types::{BadRequest, ErrorDetails, FieldViolation, LocalizedMessage, StatusExt};
 use validator::Validate;
 
 pub trait ValidateExt: Validate {
@@ -9,26 +9,37 @@ pub trait ValidateExt: Validate {
         let caller = Location::caller();
 
         self.validate().map_err(|err| {
-            let message = err
-                .field_errors()
-                .iter()
-                .flat_map(|(field, errs)| {
-                    errs.iter().map(move |err| {
-                        err.message
-                            .as_deref()
-                            .map(|m| format!("{field}:{m}"))
-                            .unwrap_or_else(|| format!("{field}: invalid"))
-                    })
-                })
-                .collect::<Vec<String>>()
-                .join(", ");
+            let mut error_details = ErrorDetails::new();
 
-            Status::invalid_argument(format!(
-                "{} (at {}:{})",
-                message,
-                caller.file(),
-                caller.line()
-            ))
+            for (field, errors) in err.field_errors() {
+                for error in errors {
+                    let code = error.code.as_ref();
+                    error_details.add_bad_request_violation(field.to_string(), code.to_string());
+                }
+            }
+
+            let err_message = err.to_string();
+
+            error_details
+                .add_help_link("Api Link", "https://api.jamsrpay.com")
+                .set_localized_message("en-US", err_message.to_string());
+
+            let mut metadata = MetadataMap::new();
+            metadata.insert(
+                "location",
+                format!("{}:{}", caller.file(), caller.line())
+                    .parse()
+                    .unwrap(),
+            );
+
+            Status::with_error_details_and_metadata(
+                Code::InvalidArgument,
+                err_message,
+                error_details,
+                metadata,
+            )
         })
     }
 }
+
+impl<T: Validate> ValidateExt for T {}
